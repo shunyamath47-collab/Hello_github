b127580a2cc83f7efa31e3feead9e84a73fff1ec
shunyamath47-collab <shunya.math47@gmail.com>
2026-07-01 23:15:52 +0900

    my first modifier

---
+++ hello_git.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Hello,git\n");
+    printf("Hello,repository\n");
+    printf("Hello,enginieer\n");
+    return 0;
+    
+}
--- hello_github.c
+++ hello_github.c
@@ -2,4 +2,5 @@
 
 int main(void){
   printf("hello,github");
+  return 0;
 }
